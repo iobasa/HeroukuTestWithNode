b47056c13ede6691e398205b4c61b71e16f2e6c9
Obasa <i_obasa@ats-hr420-dm17.fc.ca>
2019-09-30 18:06:26 -0400

    adding hbs starting doc

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 // herouku assigns a port when it deploys win the process (environmentvariables - coming)
 // locally this will run @ port 3000; remotely it'll run whenever herouku tells it to run
@@ -7,15 +8,30 @@ const port = process.env.PORT || 3000; // a double pipe => || means "or"
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+// tell express to use the handlebars engine to render data
+app.set('view engine', 'hbs');
+
+// tell express to use the views folder to find its templates
+app.set('views', __dirname + '/views');
+
+// a forward slash is the home route (same as index.html)
 app.get('/', (req, res) => {
-  console.log('at the contact route')
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  console.log('at the home route');
+
+  //res.sendFile(path.join(__dirname + '/views/index.html'));
+
+  res.render('home', { message: "hi there!", anothermessage: "This is easy!" } );
+  //this builds localhost:3000/views/index.html
 })
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+  console.log('at the contact route');
+
+  //res.sendFile(path.join(__dirname + '/views/contact.html'));
+
+  res.render('contact', { message: "what is your name?" } );
 })
 
 app.listen(port, () => {
