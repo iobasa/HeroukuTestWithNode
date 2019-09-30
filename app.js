4fe4dcb958d901cc6b7b5c16064be6239b3c4ff8
Obasa <i_obasa@ats-hr420-dm17.fc.ca>
2019-09-30 19:50:36 -0400

    database files review

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const path = require('path');
+const path = require('path'); // path lets us navigate the file
 const hbs = require('hbs');
+
+// require the sql connection file
+const sql = require('./utils/sql')
 
 // herouku assigns a port when it deploys win the process (environmentvariables - coming)
 // locally this will run @ port 3000; remotely it'll run whenever herouku tells it to run
@@ -32,6 +35,26 @@ app.get('/contact', (req, res) => {
   //res.sendFile(path.join(__dirname + '/views/contact.html'));
 
   res.render('contact', { message: "what is your name?" } );
+})
+
+app.get('/users', (req, res) => {
+  sql.getConnection((err, connection) => {
+    if (err) {
+      return console.log(err.message);
+    }
+
+    let query = `SELECT * FROM tbl_card`;
+
+    sql.query(query, (err, rows) => {
+      connection.release();
+
+      if (err) { return console.log(err.message)}
+
+      console.log(rows);
+
+      res.render('user', rows[0]);
+    })
+  })
 })
 
 app.listen(port, () => {
